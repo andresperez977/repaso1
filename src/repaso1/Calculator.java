054a383fda2a2db32b16634420b00bb50cccf7ba
Andres <andresperezperales@gmail.com>
2026-05-18 13:15:17 +0200

    repaso

---
+++ src/repaso1/Calculator.java
@@ -0,0 +1,16 @@
+package repaso1;
+
+public class Calculator {
+	public static float modulus(float num1, int num2) {
+		return num1 %num2;
+	}
+
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	public static double division(double num1, double num2) {
+		return num1 / num2;
+	}
+}
+ 
+
---
+++ src/repaso1/VariableTest.java
@@ -0,0 +1,23 @@
+package repaso1;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// primitive variables(tipos mas sencillos)
+		var num1 = 1.0;
+		int num2 = 47;
+		var resultado = (int)num1 + num2;
+		System.out.println(resultado);
+		resultado =Calculator. sum((int)num1, num2);
+
+		double result2 = num1 / num2;
+		System.out.println(result2);
+
+		// reference variables
+		
+		result2 = Calculator.division(num1, num2);
+		System.out.println(result2);
+		System.out.println( Calculator.modulus(3.2f,3));
+
+	}
+}
