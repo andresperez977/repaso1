b2cd5d9e16c1970f05ef03eee2e666437d61bfdb
Andres <andresperezperales@gmail.com>
2026-05-19 10:51:14 +0200

    seguimiento de repaso

---
+++ src/repaso1/Algorithm1.java
@@ -0,0 +1,42 @@
+package repaso1;
+
+import java.util.Scanner;
+
+public class Algorithm1 {
+
+	public static void main(String[] args) {
+		// inputs : 3 numbers, can be represented b 3 variables
+
+		int a, b, c;
+		Scanner sc = new Scanner(System.in);// leer datos de consola
+		a = sc.nextInt();// leer linea de dato y converti linea a int
+		b = sc.nextInt();
+		c = sc.nextInt();
+		
+		 Long currentTime=System.nanoTime();
+		System.out.println("the result is" + combineDigits(a, b, c));
+		 Long endTime=System.nanoTime();
+		sc.close();
+	}
+/**
+ * a the highest digit
+ * @param a
+ * @param b
+ * @param c the lowest digit 
+ * @return the combined integer 
+ */
+	private static int combineDigits(int a, int b, int c) {
+		// logical operator: or ||,and &&, not !
+		// Validation
+		if (a > 9 || a < 0 || b < 9 || b < 0 || c > 9 || c < 0) {
+			System.out.println("Your number should be single digit and non negative");
+			return 0;
+		}
+		// a=a * 100;
+		a *= 100;
+		b *= 10;
+		// output
+		return a + b + c;
+
+	 }
+}
--- src/repaso1/VariableTest.java
+++ src/repaso1/VariableTest.java
@@ -6,18 +6,25 @@ public class VariableTest {
 		// primitive variables(tipos mas sencillos)
 		var num1 = 1.0;
 		int num2 = 47;
-		var resultado = (int)num1 + num2;
+		var resultado = (int) num1 + num2;
 		System.out.println(resultado);
-		resultado =Calculator. sum((int)num1, num2);
+		resultado = Calculator.sum((int) num1, num2);
 
 		double result2 = num1 / num2;
 		System.out.println(result2);
 
 		// reference variables
-		
+
 		result2 = Calculator.division(num1, num2);
 		System.out.println(result2);
-		System.out.println( Calculator.modulus(3.2f,3));
+		System.out.println(Calculator.modulus(3.2f, 3));
+
+		boolean stop = false;
+		String color = "green";
+		if (color == "green") {
+			stop = false; 
+
+		}
 
 	}
 }
